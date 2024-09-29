8f6a25739f63178b4befe4aff8813c28b2a9ec09
Guilherme Rondon <54502342+guilhermerondon@users.noreply.github.com>
2024-09-28 23:36:21 -0300

    Atualizar o SpotifyController.cs

--- Controllers/SpotifyController.cs
+++ Controllers/SpotifyController.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-using ApiSpotifyReadme.Services; // Ajuste para o namespace correto
+using ApiSpotifyReadme.Services;
 using System.Threading.Tasks;
 
 namespace ApiSpotifyReadme.Controllers
